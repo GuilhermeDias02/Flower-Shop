f5e49ec8f69223796daab1f7e1ab78aca7297d38
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-09-15 21:17:26 +0200

    config: config projet et début architecture

---
+++ src/app/features/flowers/models/flowers.model.ts
@@ -0,0 +1,16 @@
+export interface Flower {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  origin: string;
+  bouquet: boolean;
+}
+
+export interface AddFlowerDTO {
+  title: string;
+  description: string;
+  price: number;
+  origin: string;
+  bouquet: boolean;
+}
