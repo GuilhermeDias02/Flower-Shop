00d45dc7b45133e2e382db2522c3aa5e621bbf78
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-09-23 14:35:30 +0200

    feat: save and read cart from local storage

--- src/app/features/cart/models/cart.model.ts
+++ src/app/features/cart/models/cart.model.ts
@@ -2,3 +2,8 @@ export interface Cart {
   flowers: Map<number, number>; // flowerId -> quantity
   userId: number;
 }
+
+export interface CompatibleCart {
+  flowers: [number, number][];
+  userId: number;
+}
--- src/app/features/cart/services/cart.service.ts
+++ src/app/features/cart/services/cart.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
-import { Cart } from '../models/cart.model';
+import { Cart, CompatibleCart } from '../models/cart.model';
 import { AuthService } from '../../auth/services/auth.service';
 
 @Injectable({
@@ -26,6 +26,10 @@ export class CartService {
   cartCount = signal<number>(
     this.getUserCart(this.authService.currentUser$()?.id)?.flowers.size ?? 0,
   );
+
+  constructor() {
+    if (this.loadCartsFromStorage()) console.warn('Carts loaded from local storage');
+  }
 
   //Returns the current user's cart or false if it's empty
   getUserCart(userId?: number): Cart | null {
@@ -63,6 +67,50 @@ export class CartService {
     if (cart) {
       const currentCartIndex = this.carts.findIndex((c) => c.userId === cart.userId);
       this.carts[currentCartIndex] = cart;
+      this.saveCartsToStorage();
     }
+  }
+
+  private saveCartsToStorage(): void {
+    localStorage.setItem('carts', JSON.stringify(this.carts.map((c) => this.cartMapToArray(c))));
+  }
+
+  private loadCartsFromStorage(): boolean {
+    const storedCarts: string | null = localStorage.getItem('carts');
+
+    if (storedCarts) {
+      const compatibleCarts = JSON.parse(storedCarts) as CompatibleCart[];
+      this.carts = compatibleCarts.map((cc) => this.cartArrayToMap(cc));
+      return true;
+    } else {
+      this.saveCartsToStorage();
+      return false;
+    }
+  }
+
+  cartMapToArray(cart: Cart): CompatibleCart {
+    if (cart) {
+      return {
+        flowers: Array.from(cart.flowers.entries()),
+        userId: cart.userId,
+      };
+    }
+    return {
+      flowers: [],
+      userId: 0,
+    };
+  }
+
+  cartArrayToMap(compatibleCart: CompatibleCart): Cart {
+    if (compatibleCart) {
+      return {
+        flowers: new Map<number, number>(compatibleCart.flowers),
+        userId: compatibleCart.userId,
+      };
+    }
+    return {
+      flowers: new Map<number, number>(),
+      userId: 0,
+    };
   }
 }
